4a63d688e76a2303f72c1d18b64a356d57eab1c3
Sagar Pawar <100870856+SPinfluencers@users.noreply.github.com>
2022-09-18 13:20:00 +0530

    user app

--- User-Information-App/src/Components/Userdetails.jsx
+++ User-Information-App/src/Components/Userdetails.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import axios from 'axios'
+import style from '../Components/user.css'
 
 const Userdetails = () => {
+    const [data, setData] = React.useState([])
+    const params = useParams();
+    console.log(params)
+
+    const getData = () => {
+        return axios({
+          url: `https://62a584a5430ba53411c826d3.mockapi.io/products/${params.user_id}`,
+          method: 'GET'
+        })
+      }
+
+    React.useEffect(() => {
+       getData()
+       .then((res) => {
+        setData(res.data)
+       })
+    }, [])
+    console.log(data)
+
   return (
-    <div>Userdetails</div>
+    <div >
+        <h1>User Details</h1>
+        <div className='userdetails'>
+          <h4>User Id - {data.id}</h4>
+          <h2>{data.firstName} {data.lastName}</h2>
+          <img style={{ width: "100px" }} src={data.img} alt="" />
+          <p>{data.details}</p>
+          <div>
+            <Link to="/users">Go Back</Link>
+          </div>
+        </div>
+    </div>
   )
 }
 
